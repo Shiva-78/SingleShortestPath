d907ae7dd9531febd36434f6961b48cb7814ba7d
Shivam Mishra <58064491+Shiva-78@users.noreply.github.com>
2021-11-28 21:46:41 -0600

    Initial Commit

---
+++ src/main/java/com/Algo/SingleShortestPath/SingleShortestPathApplication.java
@@ -0,0 +1,13 @@
+package com.Algo.SingleShortestPath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingleShortestPathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingleShortestPathApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/Algo/SingleShortestPath/bellman_ford/NodeWrapper.java
@@ -0,0 +1,51 @@
+package com.Algo.SingleShortestPath.bellman_ford;
+
+/**
+ * Data structure containing a node, it's total cost from the start and its predecessor.
+ *
+ * <p>Used by {@link BellmanFord}.
+ */
+class NodeWrapper<N> {
+  private final N node;
+  private int totalCostFromStart;
+  private NodeWrapper<N> predecessor;
+
+  NodeWrapper(N node, int totalCostFromStart, NodeWrapper<N> predecessor) {
+    this.node = node;
+    this.totalCostFromStart = totalCostFromStart;
+    this.predecessor = predecessor;
+  }
+
+  N getNode() {
+    return node;
+  }
+
+  void setTotalCostFromStart(int totalCostFromStart) {
+    this.totalCostFromStart = totalCostFromStart;
+  }
+
+  public int getTotalCostFromStart() {
+    return totalCostFromStart;
+  }
+
+  public void setPredecessor(NodeWrapper<N> predecessor) {
+    this.predecessor = predecessor;
+  }
+
+  public NodeWrapper<N> getPredecessor() {
+    return predecessor;
+  }
+
+  // Using identity for equals and hashcode here, which is much faster.
+  // It's sufficient as within the algorithm, we have only one NodeWrapper instance per node.
+
+  @Override
+  public boolean equals(Object other) {
+    return super.equals(other);
+  }
+
+  @Override
+  public int hashCode() {
+    return super.hashCode();
+  }
+}
